10cb1f15e61587733a4902f819a30a8cbf63b304
viniciuslo66 <viniciuslo66@gmail.com>
2022-05-18 20:57:21 -0300

    Configuração Iniciais

---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Router, BrowserRouter, Routes } from "react-router-dom";
+
+import Login from "../pages/login";
+import Home from "../pages/home";
+import Adm from "../pages/adm";
+import Busca from "../pages/busca";
+import Cadastro from "../pages/cadastro";
+import ConfigUsuario from "..pages/configUsuario"
+
+
+
+function Rotas() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Router element = {<Login /> } path="/" exact />
+        <Router element = {<Home /> } path="/entrada" />
+        <Router element = {<Adm /> } path="/adm" />
+        <Router element = {<Busca /> } path="/busca" />
+        <Router element = {<Cadastro /> } path="/cadastro" />
+        <Router element = {<ConfigUsuario /> } path="/configUsuario" />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Rotas;
+
